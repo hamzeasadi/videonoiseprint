0298494a1376578cb40722639ca55a1b90d1dfe6
hamzeasadi <hamzeasadi1@gmail.com>
2023-02-16 11:00:01 +0000

    all upload

--- utils.py
+++ utils.py
@@ -86,15 +86,15 @@ class OneClassLoss(nn.Module):
         self.bs = batch_size
         self.nc = num_cams
         self.reg = reg
-        self.m = calc_m(batch_size=batch_size, numcams=num_cams, m1=m1, m2=m2)
+        # self.m = calc_m(batch_size=batch_size, numcams=num_cams, m1=m1, m2=m2)
         # distmtxdim = num_cams * (batch_size//num_cams)
         # self.m = margin*torch.ones(size=(distmtxdim, distmtxdim), device=dev, dtype=torch.float32)
-        self.lbls = calc_labels(batch_size=batch_size, numcams=num_cams, m1=m1, m2=m2)
+        # self.lbls = calc_labels(batch_size=batch_size, numcams=num_cams, m1=m1, m2=m2)
         # for i in range(self.lbls.size()[0]):
         #     self.lbls[i,i] = 0
 
         self.crt = nn.BCEWithLogitsLoss()
-        self.newloss = loss2.SoftMLoss(batch_size=batch_size, framepercam=batch_size//num_cams)
+        self.newloss = loss2.SoftMLoss(batch_size=batch_size, framepercam=batch_size//num_cams, m1=m1, m2=m2)
         # self.crt = nn.BCELoss(reduction='mean')
 
     def forward(self, X):
