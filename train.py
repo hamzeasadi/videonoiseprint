9f909f1155a3a8a70fd525fba5502ed138bf5c4e
hamzeasadi <hamzeasadi1@gmail.com>
2023-05-16 08:20:28 +0100

    all good

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 if __name__ == '__main__':
-    epochs = 100
+    epochs = 1000
     lr = 1e-3
     batch_size = 256
     num_cls = 28
